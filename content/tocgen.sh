39397b378a336fed536f383c5bd0991511a34116
Kurogamu <ahlbom.daniel@gmail.com>
2016-08-08 14:12:34 +0200

    Modified script, should work for all links now

--- content/tocgen.sh
+++ content/tocgen.sh
@@ -1,33 +1,48 @@
 #!/bin/sh
-# Small script for generating markdown toc
-# Currently looks for header containing phrase "Innehåll"
-# Should be moved to a variable, once adequate sed-skills are aqcuired
+# Small script for generating markdown toc.
+# Change variables to suit your needs.
+# Assumes TOC is marked by a header, followed by another header.
+# Note that all text between these is deleted, recovery is possible via backed up files.
 
-for i in $( ls *.md ); do
+
+# Modify this variable for the files your looking for
+files=`ls *.md`
+
+# This searches for keyword. Note that regex is handled by both sed and awk
+keyword="#*# Innehåll"
+
+
+for i in $files; do
     # Backup files in case of screw-up
     cp $i ${i}.tocbk
-    # Grep all lines starting with `##` (assume headers)
-    # Remove header with "Innehåll"
-    # Substitute all h2 with `+`
-    # Substitute the rest of `#` with `\t`
-    # Place header-title in square brackets, and lower-case copy following `(#`
-    # Remove all non-alnum/whitespace following `(#`
-    # Replace all whitespaces with `-`
-    # Add closing `)` to end of all lines
+
+    # 1. Grep all lines starting with `##` (assume headers)
+    # 2. Remove line containing keyword
+    # 3. Substitute all h2 with `+`
+    # 4. Substitute the rest of `#` with `\t`
+    # 5. Place header-title in square brackets, and lower-case copy following `(#`
+    # 6. This line is *icky*, point is we want to keep alnum and *some* special chars (eg / and -)
+    #    We do this by matching a string of desired characters until it hits an undesired one and removing it
+    #    (I suspect learning awk would solve this, but meh)
+    # 7. Replace all whitespaces and remaining special chars with `-`
+    # 8. Reduce >1 occurences with `-` to single `-`
+    # 9. Add closing `)` to end of all lines
     contents=`grep '##' $i \
-            | sed 's/#\+ Innehåll//g' \
+            | sed "s/$keyword//g" \
             | sed 's/##\s\+/+ /g' \
             | sed 's/#/\t/g' \
             | sed 's/+\s\(.*\)/+ [\1](#\L\1/g' \
-            | sed ':a; s/(#\([[:alnum:] ]*\)[[:punct:]]/(#\1/g; ta' \
-            | sed ':a; s/(#\([^ ][^ ]*\)\s/(#\1-/g; ta' \
+            | sed ':a; s/(#\([[:alnum:] /-]*\)[()*_?,.:]/(#\1/g; ta' \
+            | sed ':a; s/(#\([^ ][^ ]*\)[/ ]/(#\1-/g; ta' \
+            | sed ':a; s/(#\([^ ][^ ]*\)--\+/(#\1-/g; ta' \
             | sed 's/(#[^ ][^ ]*/&)/g' `
-    # Remove all lines between "Innehåll" and first following line starting with `#`
-    # Place toc after lLine containing "Innehåll"
+
+    # Remove all lines between keyword and first following line starting with `#`
+    # Place toc after line containing keyword
     # Write to temporary file
-    sed '/Innehåll/,/^#/{/Innehåll/!{/^#/!D}}' $i \
+    sed "/$keyword/,/^#/{/$keyword/!{/^#/!D}}" $i \
         | awk -v toc="\n${contents}\n" \
-            '!found && /## Innehåll/ {print; print toc; next; found=1 } 1' \
+            "!found && /$keyword/ {print; print toc; next; found=1 } 1" \
         > ${i}.toc
     # Move temporary file to new one (fugly workaround, but `> ${i}` doesn't appear to work)
     mv ${i}.toc $i
